9cecedb0b43e3bd1cb934f98877cf5c1dcd87294
aadhityakumaran <kumaranpoke@gmail.com>
2024-08-07 23:00:37 +0530

    Literals

--- scanner.c
+++ scanner.c
@@ -21,6 +21,10 @@ static bool isAtEnd() {
     return *scanner.current == '\0';
 }
 
+static bool isDigit(char c) {
+    return c >= '0' && c <= '9';
+}
+
 static Token makeToken(TokenType type) {
     Token token;
     token.type = type;
@@ -86,6 +90,29 @@ static void skipWhitespace() {
     }
 }
 
+static Token string() {
+    while (peek() != '"' && !isAtEnd()) {
+        if (peek() == '\n') scanner.line++;
+        advance();
+    }
+
+    if (isAtEnd()) return errorToken("Unterminated string.");
+
+    advance();
+    return makeToken(TOKEN_STRING);
+}
+
+static Token number() {
+    while (isDigit(peek())) advance();
+
+    if (peek() == '.' && isDigit(peekNext())) {
+        advance();
+        while (isDigit(peek())) advance();
+    }
+
+    return makeToken(TOKEN_NUMBER);
+}
+
 Token scanToken() {
     skipWhitespace();
     scanner.start = scanner.current;
@@ -93,6 +120,7 @@ Token scanToken() {
     if (isAtEnd()) return makeToken(TOKEN_EOF);
 
     char c = advance();
+    if (isDigit(c)) return number();
 
     switch (c) {
         case '(': return makeToken(TOKEN_LEFT_PAREN);
@@ -113,6 +141,7 @@ Token scanToken() {
             return makeToken(match('=') ? TOKEN_LESS_EQUAL : TOKEN_LESS);
         case '>':
             return makeToken(match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
+        case '"': return string();
     }
 
     return errorToken("Unexpected character.");
